674bc37a9cd15ce08513705c0af4ee2e6fb8c46f
aziis98 <antonio.delucreziis@gmail.com>
2018-07-20 18:40:47 +0200

    Fixed gitignore

--- rls/debug/deps/icfp_2018-9d71d3f8630bcddc.d
+++
@@ -1,5 +0,0 @@
-c:\Users\aziis98\Documents\Develop\Rust\icfp-2018\rls\debug\deps\icfp_2018-9d71d3f8630bcddc.rmeta: src\main.rs
-
-c:\Users\aziis98\Documents\Develop\Rust\icfp-2018\rls\debug\deps\icfp_2018-9d71d3f8630bcddc.d: src\main.rs
-
-src\main.rs:
--- rls/debug/libicfp-2018.d
+++
@@ -1,1 +0,0 @@
-c:\Users\aziis98\Documents\Develop\Rust\icfp-2018\rls\debug\libicfp-2018.rmeta: c:\Users\aziis98\Documents\Develop\Rust\icfp-2018\src\main.rs
