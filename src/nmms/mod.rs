1dd97cc0ec3a72082f687e528ef9ce9676020812
aziis98 <antonio.delucreziis@gmail.com>
2018-07-21 15:32:46 +0200

    Starting regions

--- src/nmms/mod.rs
+++ src/nmms/mod.rs
@@ -15,26 +15,10 @@ pub struct CoordinateDifference {
 	pub dz: i16
 }
 
-impl CoordinateDifference {
-	pub fn mlen(&self) -> u16 {
-		(self.dx.abs() + self.dz.abs() + self.dz.abs()) as u16
-	}
-
-	pub fn clen(&self) -> u16 {
-		self.dx.abs().max(self.dy.abs()).max(self.dz.abs()) as u16
-	}
-}
-
-impl Sub for Coordinate {
-	type Output = CoordinateDifference;
-
-	fn sub(self, other: Coordinate) -> CoordinateDifference {
-		CoordinateDifference {
-			dx: other.x as i16 - self.x as i16,
-			dy: other.y as i16 - self.y as i16,
-			dz: other.z as i16 - self.z as i16,
-		}
-	}
+#[derive(Debug, Copy, Clone)]
+pub struct Region {
+	pub c1: Coordinate,
+	pub c2: Coordinate
 }
 
 impl Add<CoordinateDifference> for Coordinate {
@@ -46,5 +30,35 @@ impl Add<CoordinateDifference> for Coordinate {
 			y: (self.y as i16 + d.dy) as u8,
 			z: (self.z as i16 + d.dz) as u8,
 		}
+	}
+}
+
+impl Sub for Coordinate {
+	type Output = CoordinateDifference;
+
+	fn sub(self, other: Coordinate) -> CoordinateDifference {
+		CoordinateDifference {
+			dx: self.x as i16 - other.x as i16,
+			dy: self.y as i16 - other.y as i16,
+			dz: self.z as i16 - other.z as i16,
+		}
+	}
+}
+
+impl Coordinate {
+	
+	pub fn is_adjacent(self, other: Coordinate) -> bool {
+		(self - other).mlen() == 1
+	}
+	
+}
+
+impl CoordinateDifference {
+	pub fn mlen(&self) -> u16 {
+		(self.dx.abs() + self.dz.abs() + self.dz.abs()) as u16
+	}
+
+	pub fn clen(&self) -> u16 {
+		self.dx.abs().max(self.dy.abs()).max(self.dz.abs()) as u16
 	}
 }
