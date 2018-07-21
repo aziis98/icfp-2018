427bf0bfa176ff76c0e739366ffc5dbe15b217e6
aziis98 <antonio.delucreziis@gmail.com>
2018-07-21 15:47:07 +0200

    Regions

--- src/nmms/mod.rs
+++ src/nmms/mod.rs
@@ -47,6 +47,12 @@ impl Sub for Coordinate {
 
 impl Coordinate {
 	
+	pub fn new(x: u8, y: u8, z: u8) -> Coordinate {
+		Coordinate { 
+			x, y, z
+		}
+	}
+
 	pub fn is_adjacent(self, other: Coordinate) -> bool {
 		(self - other).mlen() == 1
 	}
@@ -60,5 +66,37 @@ impl CoordinateDifference {
 
 	pub fn clen(&self) -> u16 {
 		self.dx.abs().max(self.dy.abs()).max(self.dz.abs()) as u16
+	}
+}
+
+impl Region {
+	pub fn new(c1: Coordinate, c2: Coordinate) -> Region {
+		let min_c = Coordinate::new(
+			c1.x.min(c2.x),
+			c1.y.min(c2.y),
+			c1.z.min(c2.z)
+		);
+		let max_c = Coordinate::new(
+			c1.x.max(c2.x),
+			c1.y.max(c2.y),
+			c1.z.max(c2.z)
+		);
+
+		Region {
+			c1: min_c,
+			c2: max_c
+		}
+	}
+
+	pub fn is_member(self, c: Coordinate) -> bool {
+		(self.c1.x <= c.x && c.x <= self.c2.x) &&
+			(self.c1.y <= c.y && c.y <= self.c2.y) &&
+			(self.c1.z <= c.z && c.z <= self.c2.z)
+	}
+
+	pub fn dimensions(self) -> u8 {
+		(if self.c1.x == self.c2.x { 0 } else { 1 }) 
+			+ (if self.c1.y == self.c2.y { 0 } else { 1 })
+			+ (if self.c1.z == self.c2.z { 0 } else { 1 })
 	}
 }
