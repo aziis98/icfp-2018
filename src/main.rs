28150edf1d0258a8b396be2353790e2264cf43b0
aziis98 <antonio.delucreziis@gmail.com>
2018-07-21 12:36:39 +0200

    Added Coordinate & CoordinateDistance structs and Add trait for them

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 
-
+pub mod nmms;
 
 fn main() {
-    
+    let c = nmms::Coordinate { x: 1, y: 2, z: 3 };
 }
--- src/nmms/mod.rs
+++ src/nmms/mod.rs
@@ -1,6 +1,28 @@
 
-mod nmms {
+use std::ops::Add;
 
-	type Coordinate = [u8; 3];
+#[derive(Debug, Copy, Clone)]
+pub struct Coordinate {
+	pub x: u8,
+	pub y: u8,
+	pub z: u8
+}
 
+#[derive(Debug, Copy, Clone)]
+pub struct CoordinateDifference {
+	pub dx: i16,
+	pub dy: i16,
+	pub dz: i16
+}
+
+impl Add<CoordinateDifference> for Coordinate {
+	type Output = Coordinate;
+
+	fn add(self, d: CoordinateDifference) -> Coordinate {
+		Coordinate {
+			x: (self.x as i16 + d.dx) as u8,
+			y: (self.y as i16 + d.dy) as u8,
+			z: (self.z as i16 + d.dz) as u8,
+		}
+	}
 }
